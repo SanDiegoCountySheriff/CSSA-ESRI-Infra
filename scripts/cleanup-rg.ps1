fc4eadff33d49a3928f299503397281a6e937ea5
Andrew Tangeman <andrew@quarticsolutions.com>
2023-06-22 15:25:05 -0700

    working on connection to gw

--- scripts/cleanup-rg.ps1
+++ scripts/cleanup-rg.ps1
@@ -6,8 +6,11 @@
     #[Parameter(Mandatory=$true)]
     #[string]$subscriptionId = ""
  )
+
+$id = convertfrom-json (az account list --query "[?isDefault].id | [0]")
+
 # Set the subscription.
-#Set-AzContext -SubscriptionId $subscriptionId
+Set-AzContext -SubscriptionId $id
 
 # List all resource groups that will be removed.
 Get-AzResourceGroup | ? ResourceGroupName -match $rg | Select-Object ResourceGroupName
