d22fe9d1fe61206b19c2a8ac1c22778b8bbcad6d
Andrew Tangeman <andrew@quarticsolutions.com>
2023-06-22 16:16:50 -0700

    working on cleanup script

--- scripts/cleanup-rg.ps1
+++ scripts/cleanup-rg.ps1
@@ -7,8 +7,6 @@
     #[string]$subscriptionId = ""
  )
 
-Get-AzContext -ListAvailable | Where{$_.Name -match 'DS - GIS - DEVTEST'} | Set-AzContext 
-
 # List all resource groups that will be removed.
 #Get-AzResourceGroup | ? ResourceGroupName -match $rg | Select-Object ResourceGroupName
 Get-AzResourceGroup -Name $rg -ErrorVariable notPresent -ErrorAction SilentlyContinue
@@ -20,7 +18,8 @@ else {
     $rg_resource_location = ($rg_resource).Location
 
     Start-ThreadJob {
-        Get-AzContext -ListAvailable | Where{$_.Name -match 'DS - GIS - DEVTEST'} | Set-AzContext 
+        Connect-AzAccount -ServicePrincipal -Tenant 'cc3e0e2d-a2a6-4495-97d5-8999e9bb2aa3' -Credential System.Management.Automation.PSCredential -Environment AzureCloud @processScope
+        Set-AzContext -SubscriptionId '66d233df-ad0c-45a4-a6bc-d77919e18237' -TenantId 'cc3e0e2d-a2a6-4495-97d5-8999e9bb2aa3'
         Remove-AzResourceGroup -Name $rg_resource_name -AsJob -Force
     
     } | Receive-Job -Wait -AutoRemoveJob
