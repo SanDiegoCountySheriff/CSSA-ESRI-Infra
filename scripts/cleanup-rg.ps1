db7329bc3d5dd676fbe33e15cf500420639ed565
Andrew Tangeman <andrew@quarticsolutions.com>
2023-06-22 16:20:20 -0700

    working on cleanup script

--- scripts/cleanup-rg.ps1
+++ scripts/cleanup-rg.ps1
@@ -14,9 +14,11 @@ if ($notPresent) {
     exit(0)
 }
 else {
+    $rg_resource = Get-AzResourceGroup -Name $rg 
     $rg_resource_name = ($rg_resource).ResourceGroupName 
     $rg_resource_location = ($rg_resource).Location
 
-    Remove-AzResourceGroup -Name $rg_resource_name -AsJob -Force | Receive-Job -Wait -AutoRemoveJob
+    Remove-AzResourceGroup -Name $rg_resource_name -AsJob -Force
+    # | Receive-Job -Wait -AutoRemoveJob
 }
 
