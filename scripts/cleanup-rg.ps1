0b5a05d7d6870b767a75dff9e351bc370c25b09e
Andrew Tangeman <andrew@quarticsolutions.com>
2023-06-22 16:18:20 -0700

    working on cleanup script

--- scripts/cleanup-rg.ps1
+++ scripts/cleanup-rg.ps1
@@ -17,11 +17,6 @@ else {
     $rg_resource_name = ($rg_resource).ResourceGroupName 
     $rg_resource_location = ($rg_resource).Location
 
-    Start-ThreadJob {
-        Connect-AzAccount -ServicePrincipal -Tenant 'cc3e0e2d-a2a6-4495-97d5-8999e9bb2aa3' -Credential System.Management.Automation.PSCredential -Environment AzureCloud @processScope
-        Set-AzContext -SubscriptionId '66d233df-ad0c-45a4-a6bc-d77919e18237' -TenantId 'cc3e0e2d-a2a6-4495-97d5-8999e9bb2aa3'
-        Remove-AzResourceGroup -Name $rg_resource_name -AsJob -Force
-    
-    } | Receive-Job -Wait -AutoRemoveJob
+    Remove-AzResourceGroup -Name $rg_resource_name -AsJob -Force | Receive-Job -Wait -AutoRemoveJob
 }
 
