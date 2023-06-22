a7e64d7fd1f5899ac07db3845ed39d8fa00db6f0
Andrew Tangeman <andrew@quarticsolutions.com>
2023-06-22 11:57:37 -0700

    added cleanup script

--- scripts/cleanup-rg.ps1
+++ scripts/cleanup-rg.ps1
@@ -1,13 +1,16 @@
 # Clean up PowerShell resources.
 
-$prefix = 'rg-cosm-gis-env-test'
-$subscriptionId = '66d233df-ad0c-45a4-a6bc-d77919e18237'
-
+ param (
+    [Parameter(Mandatory=$true)]
+    [string]$rg = ""
+    [Parameter(Mandatory=$true)]
+    [string]$subscriptionId = ""
+ )
 # Set the subscription.
 Set-AzContext -SubscriptionId $subscriptionId
 
 # List all resource groups that will be removed.
-Get-AzResourceGroup | ? ResourceGroupName -match $prefix | Select-Object ResourceGroupName
+Get-AzResourceGroup | ? ResourceGroupName -match $rg | Select-Object ResourceGroupName
 
 # Remove the resource groups shown in the preceding command.
-Get-AzResourceGroup | ? ResourceGroupName -match $prefix | Remove-AzResourceGroup -AsJob -Force
+Get-AzResourceGroup | ? ResourceGroupName -match $rg | Remove-AzResourceGroup -AsJob -Force
