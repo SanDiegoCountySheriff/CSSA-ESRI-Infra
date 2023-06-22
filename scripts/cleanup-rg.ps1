b8b9480a301be1e00f0f5bd8504c8de254a6b16a
Andrew Tangeman <andrew@quarticsolutions.com>
2023-06-22 15:43:58 -0700

    working on cleanup script

--- scripts/cleanup-rg.ps1
+++ scripts/cleanup-rg.ps1
@@ -7,13 +7,15 @@
     #[string]$subscriptionId = ""
  )
 
-$id = convertfrom-json (az account list --query "[?isDefault].id | [0]")
-
-# Set the subscription.
-Set-AzContext -SubscriptionId $id
-
 # List all resource groups that will be removed.
-Get-AzResourceGroup | ? ResourceGroupName -match $rg | Select-Object ResourceGroupName
+#Get-AzResourceGroup | ? ResourceGroupName -match $rg | Select-Object ResourceGroupName
+$rg_resource = Get-AzResourceGroup -Name $rg
+
+$rg_resource_name = ($rg_resource).ResourceGroupName 
+$rg_resource_location = ($rg_resource).Location
+
+Remove-AzResourceGroup -Name $rg_resource_name -AsJob -Force
+
+New-AzResourceGroup -Name ResourceGroupName -Location $rg_resource_location
 
 # Remove the resource groups shown in the preceding command.
-Get-AzResourceGroup | ? ResourceGroupName -match $rg | Remove-AzResourceGroup -AsJob -Force
