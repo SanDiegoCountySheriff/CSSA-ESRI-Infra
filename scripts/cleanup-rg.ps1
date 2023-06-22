25661e29678264c4af23aa22bf68e938f16929cc
Andrew Tangeman <andrew@quarticsolutions.com>
2023-06-22 12:02:19 -0700

    added cleanup script

--- scripts/cleanup-rg.ps1
+++ scripts/cleanup-rg.ps1
@@ -3,11 +3,11 @@
  param (
     [Parameter(Mandatory=$true)]
     [string]$rg = ""
-    [Parameter(Mandatory=$true)]
-    [string]$subscriptionId = ""
+    #[Parameter(Mandatory=$true)]
+    #[string]$subscriptionId = ""
  )
 # Set the subscription.
-Set-AzContext -SubscriptionId $subscriptionId
+#Set-AzContext -SubscriptionId $subscriptionId
 
 # List all resource groups that will be removed.
 Get-AzResourceGroup | ? ResourceGroupName -match $rg | Select-Object ResourceGroupName
